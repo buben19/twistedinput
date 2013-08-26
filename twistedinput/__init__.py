7a22e47778ec433cc855cca9c2672588b7ce9d85
buben19 <ivo.slanina@gmail.com>
2013-08-26 16:26:21 +0200

    completed __init__.py file

---
+++ twistedinput/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+__title__       = 'twistedinput'
+__version__     = '0.0.1'
+__author__      = 'Ivo Slanina'
+__contact__     = 'ivo.slanina@gmail.com'
+__license__     = 'Unlicense'
+__url__         = 'https://github.com/buben19/twistedinput'
+
+from .twistedinput import *
